55a825287f00c49b56d76823a0a8140f5362aa6c
v0ss3n <m.vossen@live.nl>
2019-10-07 10:43:44 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(".carousel-model-2050").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+  $(".carousel-graduation-collection").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });  
+
+  $(".carousel-illustrations").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });  
+  
