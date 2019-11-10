7275a6ddb31b7c6db1cc25ef287be9b8c8348bee
v0ss3n <m.vossen@live.nl>
2019-11-10 12:57:50 +0100

    Do some minor changes

--- js/parallax.js
+++ js/parallax.js
@@ -2,3 +2,8 @@ var homepageScene = document.getElementById("homepage-scene");
 if (homepageScene) {
   var homepageParallax = new Parallax(homepageScene);
 }
+
+var aboutScene = document.getElementById("about-scene");
+if (aboutScene) {
+  var aboutParallax = new Parallax(aboutScene);
+}
