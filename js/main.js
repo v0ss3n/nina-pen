f64d9fb51cdb5cdfbdd083d5c254297a5d41cfed
v0ss3n <m.vossen@live.nl>
2020-02-10 12:54:49 +0100

    Improve loading screen

--- js/main.js
+++ js/main.js
@@ -97,56 +97,56 @@
 // }
 // }
 
-  $(".carousel-model-2050").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false
-  });  
+$(".carousel-model-2050").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false
+});
 
-  $(".carousel-model-2050-2").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2400,
-    arrows: false
-  });
+$(".carousel-model-2050-2").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2400,
+  arrows: false
+});
 
-  $(".carousel-residents").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false
-    });    
-  
-  $(".carousel-residents2").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false
-  });  
+$(".carousel-residents").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false
+});
 
-  $(".carousel-illustrations").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false
-  });  
+$(".carousel-residents2").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false
+});
 
-  $(".carousel-illustrations2").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows: false
-  });  
+$(".carousel-illustrations").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false
+});
 
-  
- $('.carousel-heads').slick({
+$(".carousel-illustrations2").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false
+});
+
+
+$('.carousel-heads').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
@@ -179,47 +179,47 @@ $('.carousel-looks').slick({
   asNavFor: '.carousel-looks-nav'
 });
 
-  $(".carousel-prints").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false
-  });  
+$(".carousel-prints").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false
+});
 
-  $(".carousel-prints2").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows: false
-  });  
+$(".carousel-prints2").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false
+});
 
-  $(".carousel-collection").slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay: true,
-    verticalSwiping: true,
-    autoplaySpeed: 1500
-  });  
+$(".carousel-collection").slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: true,
+  verticalSwiping: true,
+  autoplaySpeed: 1500
+});
 
-  $(".carousel-sketches").slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 300,
-    arrows: false
-  });  
+$(".carousel-sketches").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 300,
+  arrows: false
+});
 
-  $(".carousel-squares").slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay: true,
-    verticalSwiping: true,
-    autoplaySpeed: 1500
-  });  
-  
-  var homepageScene = document.getElementById("homepage-scene");
+$(".carousel-squares").slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: true,
+  verticalSwiping: true,
+  autoplaySpeed: 1500
+});
+
+var homepageScene = document.getElementById("homepage-scene");
 if (homepageScene) {
   var homepageParallax = new Parallax(homepageScene);
 }
@@ -229,10 +229,10 @@ if (aboutScene) {
   var aboutParallax = new Parallax(aboutScene);
 }
 
-$(document).ready(function() {
- 
-  setTimeout(function(){
-      $('body').addClass('loaded');
+$(document).ready(function () {
+
+  setTimeout(function () {
+    $('body').addClass('loaded');
   }, 1000);
 
 });
