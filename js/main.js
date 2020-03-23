1b36bd29bde2c4b582a7ea286503b12767678a64
v0ss3n <m.vossen@live.nl>
2020-03-23 17:14:05 +0100

    Update text and order of projects and remove jessica pictures for now

--- js/main.js
+++ js/main.js
@@ -1,102 +1,3 @@
-//   function toggleVisibility1() {
-//     var x = document.getElementById("look1");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
-//   function toggleVisibility2() {
-//     var x = document.getElementById("look1");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
-//   function toggleVisibility3() {
-//     var x = document.getElementById("look1");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
-//   function toggleVisibility4() {
-//     var x = document.getElementById("look1");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
-//   function toggleVisibility5() {
-//     var x = document.getElementById("look1");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
-// function toggleVisibility6() {
-//   var x = document.getElementById("look1");
-// if (x.style.display === "none") {
-//   x.style.display = "block";
-// } else {
-//   x.style.display = "none";
-// }
-// }
-
-// function toggleVisibility7() {
-//   var x = document.getElementById("look1");
-// if (x.style.display === "none") {
-//   x.style.display = "block";
-// } else {
-//   x.style.display = "none";
-// }
-// }
-
-// function toggleVisibility8() {
-//   var x = document.getElementById("look9");
-// if (x.style.display === "none") {
-//   x.style.display = "block";
-// } else {
-//   x.style.display = "none";
-// }
-// }
-
-// function toggleVisibility9() {
-//   var x = document.getElementById("look9");
-// if (x.style.display === "none") {
-//   x.style.display = "block";
-// } else {
-//   x.style.display = "none";
-// }
-// }
-
-// function toggleVisibility10() {
-//   var x = document.getElementById("look10");
-// if (x.style.display === "none") {
-//   x.style.display = "block";
-// } else {
-//   x.style.display = "none";
-// }
-// }
-
-// function toggleVisibility11() {
-//   var x = document.getElementById("look11");
-// if (x.style.display === "none") {
-//   x.style.display = "block";
-// } else {
-//   x.style.display = "none";
-// }
-// }
-
 $(".carousel-model-2050").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -249,23 +150,8 @@ $(document).ready(function () {
 
   setTimeout(function () {
     $('body').addClass('loaded');
-  }, 1000);
+  });
 
 });
 
-"use strict";
-
-var section = document.querySelector('.skew-scroll');
-var currentPos = window.pageYOffset;
-
-var update = function update() {
-  var newPos = window.pageYOffset;
-  var diff = newPos - currentPos;
-  var speed = diff * 0.35;
-  section.style.transform = "skewY(".concat(speed, "deg)");
-  currentPos = newPos;
-  requestAnimationFrame(update);
-};
-
-update();
 
